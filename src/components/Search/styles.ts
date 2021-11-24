ff9b11a5692931302bdd09de82dcb3dc94547d11
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-24 18:28:16 +0100

    add styling for search button

---
+++ src/components/Search/styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles(() => ({
+    searchButton: {
+        backgroundColor: 'rgb(29, 155, 240)',
+        color: 'white',
+        '&:hover': {
+            backgroundColor: 'rgb(29, 155, 240)'
+        }
+    }
+}));
+
+export default styles;
