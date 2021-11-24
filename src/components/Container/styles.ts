fcf4396893134b19a7f42c4bfec8f02b9f7c0f88
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-24 14:18:03 +0100

    improve space styling between grids

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,9 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const styles = makeStyles(() => ({
     root: {
         padding: '40px'
-    },
-    cardContent: {
-        marginTop: '30px'
     }
 }));
 
