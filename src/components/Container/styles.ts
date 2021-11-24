83713a0c9addac0a29823fd4f240a0873f5a616c
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-24 18:25:58 +0100

    Add styling for title

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 const styles = makeStyles(() => ({
     root: {
         padding: '40px'
+    },
+    titleText: {
+        backgroundColor: 'rgb(29, 155, 240)'
     }
 }));
 
