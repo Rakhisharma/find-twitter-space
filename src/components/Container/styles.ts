d6d6ff737aaf43719ede70dd432297aee4650e93
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-24 14:07:37 +0100

    adjust styling for of conatiner

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 const styles = makeStyles(() => ({
     root: {
         padding: '40px'
+    },
+    cardContent: {
+        marginTop: '30px'
     }
 }));
 
