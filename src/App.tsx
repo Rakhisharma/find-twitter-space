d9428672eca3fc70fda7efbdc59b4ad09d5b10c2
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-25 17:23:00 +0100

    test twitter data

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import useStyles from './styles';
 
 import Header from './components/Header';
 import Container from './components/Container';
+import api from './api/index';
 
-const App = () => {
+const App = async () => {
     const { root } = useStyles();
+    //test api data
+    const data = await api().then(item => console.log('item', item));
+
+    console.log('data', data);
 
     return (
         <div className={root}>
