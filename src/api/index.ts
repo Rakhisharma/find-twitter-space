7343b04df1e0a2cae2bb7438bd8a81b5468bc8c1
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-28 17:23:01 +0100

    improve api call

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,20 @@
 let myHeaders = new Headers();
 const token = process.env.TWITTER_TOKEN;
 
-myHeaders.append('Authorization', `Bearer ${token}`);
+myHeaders.append(
+    'Authorization',
+    'Bearer AAAAAAAAAAAAAAAAAAAAAJK2WAEAAAAAz4J7KN5igVXLw9FdnUKnIzPwYa4%3DU35LMcCWGcwaH4gxpDHDNc5JTK5EOuyp36zx6bftxTP8YuVWce'
+);
 
 let requestOptions = {
     method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
+    headers: myHeaders
 };
+//netlify/functions/node-fetch/node-fetch.js
 
-const fetchList = () =>
-    fetch(
-        'https://api.twitter.com/2/spaces/search?query=web3&space.fields=creator_id,ended_at,host_ids,id,lang,participant_count,scheduled_start,speaker_ids,started_at,state,title,topic_ids&expansions=invited_user_ids,speaker_ids,creator_id,host_ids&user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld&topic.fields=description,id,name',
-        requestOptions as any
-    )
+const handler = () =>
+    fetch('/netlify/functions/node-fetch', requestOptions as any)
         .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        .then(result => result);
 
-export default fetchList;
+export default handler;
