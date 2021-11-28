e91318339fcc7ce9900a9e4ca986760d6339fbd4
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-28 18:34:47 +0100

    clean up- life is good

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,6 @@
-let myHeaders = new Headers();
-const token = process.env.TWITTER_TOKEN;
-
-myHeaders.append(
-    'Authorization',
-    'Bearer AAAAAAAAAAAAAAAAAAAAAJK2WAEAAAAAz4J7KN5igVXLw9FdnUKnIzPwYa4%3DU35LMcCWGcwaH4gxpDHDNc5JTK5EOuyp36zx6bftxTP8YuVWce'
-);
-
-let requestOptions = {
-    method: 'GET',
-    headers: myHeaders
-};
-//netlify/functions/node-fetch/node-fetch.js
-
 const handler = () =>
-    fetch('/netlify/functions/node-fetch', requestOptions as any)
-        .then(response => response.text())
+    fetch('/.netlify/functions/node-fetch')
+        .then(response => response.json())
         .then(result => result);
 
 export default handler;
