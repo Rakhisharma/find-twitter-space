5f852312d3aca55636b88a5b65ee369c32957d58
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-11-29 21:28:55 +0100

    improve type of responce

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
+import { mapToData } from './utils';
+
 const handler = () =>
     fetch('/.netlify/functions/node-fetch')
         .then(response => response.json())
-        .then(result => result);
+        .then(result => mapToData(result.data));
 
 export default handler;
