38332fde7a5c944d73ba16870dfd46de8c343565
TemporaryAdmin <rakhi.sharma@deliveryhero.com>
2021-12-01 21:58:58 +0100

    Add typer to improve how data is being passed in child component

--- src/components/Card/type.d.ts
+++ src/components/Card/type.d.ts
@@ -1,6 +1,16 @@
 import { SpacesData, UserData } from '../../api/type';
 
 export interface Data {
-    spaceData: SpacesData;
-    userData: UserData;
+    userId: FetchSuccessPayloadUsers['id'];
+    userName: FetchSuccessPayloadUsers['username'];
+    name: FetchSuccessPayloadUsers['name'];
+    profileImage: FetchSuccessPayloadUsers['profile_image_url'];
+    state: FetchSuccessPayloadData['state'];
+    lang: FetchSuccessPayloadData['lang'];
+    id: FetchSuccessPayloadData['id'];
+    title: FetchSuccessPayloadData['title'];
+    creatorId: FetchSuccessPayloadData['creator_id'];
+    scheduledStart: FetchSuccessPayloadData['scheduled_start'];
+    hostIds: FetchSuccessPayloadData['host_ids'];
+    speakerIds: FetchSuccessPayloadData['speaker_ids'];
 }
